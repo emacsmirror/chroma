89324b476498bdfc657079040cfbbe33d1da48a3
Nicolas Martyanoff <nicolas@n16f.net>
2024-07-16 13:31:30 +0200

    360 is not a valid hue for HLS representations
    close: #1

--- chroma.el
+++ chroma.el
@@ -25,14 +25,14 @@
 ;; This package provides utilities to manipulate colors. Compared to the
 ;; built-in color.el, chroma.el uses provides a consistent interface, always
 ;; accepting and returning colors as lists. It also uses commonly used
-;; representation for components: 8 bit values for RGB colors, 0-360 hue for
+;; representation for components: 8 bit values for RGB colors, 0-359 hue for
 ;; HSL colors.
 ;;
 ;; RGB colors are represented as lists of three elements for the red, green
 ;; and blue components. RGB components are represented as 8 bit integer.
 ;;
 ;; HSL colors are represented as lists of three elements for the hue,
-;; saturation and lightness. Hue is represented as an integer in [0, 360].
+;; saturation and lightness. Hue is represented as an integer in [0, 360).
 ;; Saturation and lightness are represented as floating point numbers in [0.0,
 ;; 1.0] rounded to two decimal places.
 
@@ -140,7 +140,9 @@ not represent a HSL color."
                ((<= 240 h 299)
                 (list x 0.0 c))
                ((<= 300 h 359)
-                (list c 0.0 x)))))
+                (list c 0.0 x))
+               (t
+                (error "Invalid hue %d" h)))))
     (mapcar (lambda (v) (round (* (+ v m) 255.0))) rgb)))
 
 (defun chroma-hsl-string-to-rgb-string (string)
